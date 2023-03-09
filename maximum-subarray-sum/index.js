b0951d4ed84307cb935f17f8a8da6c35a15f6fe6
Yan Sharipov <89582326+YanMSH@users.noreply.github.com>
2023-03-09 12:53:02 +0500

    feat: add Maximum Subarray Sum kata

---
+++ maximum-subarray-sum/index.js
@@ -0,0 +1,9 @@
+var maxSequence = function (arr) {
+  let maxSum = 0;
+  let currentSum = 0;
+  arr.forEach((num) => {
+    currentSum = Math.max(0, currentSum + num);
+    maxSum = Math.max(maxSum, currentSum);
+  });
+  return maxSum;
+};
