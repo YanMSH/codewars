a8cf1499032c05c68b2c67b663d45f3c6545ca27
YanMSH <bromazot@ya.ru>
2024-08-28 18:12:19 +0500

    fix: rewrite solution for the last kata in TypeScript

--- the-millionth-fibonacci-kata/index.ts
+++ the-millionth-fibonacci-kata/index.ts
@@ -1,17 +1,17 @@
-function fib(n) {
+function fib(n: number): bigint {
   if (n === 0 || n === 1) {
     return BigInt(n);
   } else if (n >= 2 && n % 2 === 0) {
     let halfn = n / 2;
     let fHalf = fib(halfn);
 
-    return (2n * fib(halfn - 1) + fHalf) * fHalf;
+    return BigInt((2n * fib(halfn - 1) + fHalf) * fHalf);
   } else if (n >= 2) {
     let halfn = (n + 1) / 2;
     let fHalf1 = fib(halfn - 1);
     let fHalf2 = fib(halfn);
 
-    return fHalf2 * fHalf2 + fHalf1 * fHalf1;
+    return BigInt(fHalf2 * fHalf2 + fHalf1 * fHalf1);
   } else {
     let a = 0n;
     let b = 1n;
