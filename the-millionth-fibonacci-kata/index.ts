2b59e4596ccec6cd07d89bf4a7364582bffd2db3
YanMSH <bromazot@ya.ru>
2024-08-28 18:08:28 +0500

    feat: add the-millionth-fibonacci-kata solution

---
+++ the-millionth-fibonacci-kata/index.ts
@@ -0,0 +1,23 @@
+function fib(n) {
+  if (n === 0 || n === 1) {
+    return BigInt(n);
+  } else if (n >= 2 && n % 2 === 0) {
+    let halfn = n / 2;
+    let fHalf = fib(halfn);
+
+    return (2n * fib(halfn - 1) + fHalf) * fHalf;
+  } else if (n >= 2) {
+    let halfn = (n + 1) / 2;
+    let fHalf1 = fib(halfn - 1);
+    let fHalf2 = fib(halfn);
+
+    return fHalf2 * fHalf2 + fHalf1 * fHalf1;
+  } else {
+    let a = 0n;
+    let b = 1n;
+    for (let i = 0; i > n; i--) {
+      [a, b] = [b - a, a];
+    }
+    return a;
+  }
+}
